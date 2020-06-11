50e3cac303a6630e6584f74daef333a27a68205e
Shawn Holman <the.ikick@gmail.com>
2020-06-10 21:13:26 -0400

    The menu will not close itself on mobile if you swipe up

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
 const Main = new (function () {
+	/** Pixels required to swipedown to close the menu */
+	const MENU_CLOSE_SWIPE_THRESHOLD = 50;
+
 	const states = new Map();
 	let timer;
 
@@ -13,8 +16,35 @@ const Main = new (function () {
 	}
 
 	// private methods
+	/**
+	 * Adds an event to close the menu on swipe down
+	 */
+	function closeMenuOnSwipeDown() {
+		let start;
+		let touchStart = function (e) {
+			start = e.touches[0].clientY;
+		};
+		let touchMove = function (e) {
+			let current = e.touches[0].clientY;
+
+			if (start - current > MENU_CLOSE_SWIPE_THRESHOLD) {
+				if ($('nav').hasClass('responsive-open')) {
+					toggleMenu();
+
+					$(document).off('touchstart');
+					$(document).off('touchmove');
+				}
+			}
+		};
+		$(document).off('touchstart').on('touchstart', touchStart);
+		$(document).off('touchmove').on('touchmove', touchMove);
+	}
+	
+	/**
+	 * Open and close the mobile menu depending on the state of it.
+	 */
 	function toggleMenu () {
-		$(this).toggleClass('is-active');
+		$('.menu-toggle').toggleClass('is-active');
 		if ($('nav').hasClass('slide-in')) {
 			$('nav').removeClass('slide-in');
 			setTimeout(function () {
@@ -24,11 +54,16 @@ const Main = new (function () {
 			$('nav').addClass('responsive-open');
 			setTimeout(function () {
 				$('nav').addClass('slide-in');
+				// on swipe down
+				closeMenuOnSwipeDown();
 			}, 50);
 		}
 	}
 
-	function onProfilePictureHover () {
+	/**
+	 * Activates the profile picture card
+	 */
+	function openProfileCard () {
 		clearTimeout(timer);
 		let t = $(this);
 
@@ -44,7 +79,10 @@ const Main = new (function () {
 		}, 150);
 	}
 
-	function onProfilePictureOut () {
+	/**
+	 * Closes the profile picture card
+	 */
+	function closeProfileCard () {
 		clearTimeout(timer);
 		$('.card').removeClass('fade-in');
 		timer = setTimeout(() => {
@@ -53,6 +91,10 @@ const Main = new (function () {
 		}, 200);
 	}
 
+	/**
+	 * Manages the history state and calls registered states accordingly
+	 * @param  event the state passed in through the popstate event
+	 */
 	function manageState(event) {
 		let hash = window.location.hash.substr(1);
 		let state = states.get(hash);
@@ -64,6 +106,9 @@ const Main = new (function () {
 		}
 	}
 
+	/**
+	 * Pushes a new state
+	 */
 	function pushState () {
 		let link = $(this).attr('data-link');
 		history.push(null, null, link);
@@ -73,8 +118,8 @@ const Main = new (function () {
 	$(document).ready(function () {
 		manageState(null);
 
-		$('.team-picture').on('mouseover', onProfilePictureHover);
-		$('.card').on('mouseleave', onProfilePictureOut);
+		$('.team-picture').on('mouseover', openProfileCard);
+		$('.card').on('mouseleave', closeProfileCard);
 		$('.menu-toggle').on('click', toggleMenu);
 		$('[data-link]').on('click', pushState);
 		$(window).on('popstate', manageState);
