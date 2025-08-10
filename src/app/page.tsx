763f0633c3f4e5a7249a5c79304870669da297aa
TheRyanMajd <rm97798@uga.edu>
2025-08-10 13:25:59 -0400

    Band-aid

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,13 @@
-import Image from "next/image";
+// import Image from "next/image";
+import About from "./components/About";
 import Navbar from "./components/navbar";
-
 export default function Home() {
   return (
     <>
       <Navbar />
       <About />
-      <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-        <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-          <Image
-            className="dark:invert"
-            src="/next.svg"
-            alt="Next.js logo"
-            width={180}
-            height={38}
-            priority
-          />
+      <div className="items-center justify-items-center p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+        <main className="">
         </main>
       </div>
     </>
