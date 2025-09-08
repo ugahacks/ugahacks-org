68587c579b33d44a64cf5681fd5af8d2c2b37c00
TheRyanMajd <rm97798@uga.edu>
2025-09-08 17:38:57 -0400

    changed banner to red, and moved href to 11 site

--- src/app/components/Banner.tsx
+++ src/app/components/Banner.tsx
@@ -1,9 +1,9 @@
 import Link from "next/link";
 export default function Banner() {
     return (
-        <div className="w-full bg-gradient-to-r from-purple-700 via-fuchsia-700 to-indigo-700 text-white text-center py-3 shadow-md animate-drop-in ">
+        <div className="w-full bg-red-600 text-white text-center py-3 shadow-md animate-drop-in ">
             <p className="text-sm sm:text-base font-medium">
-                <Link className="hover:text-red-400" href="https://mybyte.ugahacks.com/">
+                <Link className="duration-300 hover:text-red-300 hover:underline" href="https://11.ugahacks.com/">
                     🪄 UH11 Registration Live!
                 </Link>
             </p>
