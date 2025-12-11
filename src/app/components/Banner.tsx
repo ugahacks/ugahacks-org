e65ad0ab115b40f165ad7352dc5a4ccc51853819
Ali Batu Ozdener <98476407+probro91@users.noreply.github.com>
2025-12-11 17:34:47 -0500

    Update Banner component message and remove link
    Putting up registration again

--- src/app/components/Banner.tsx
+++ src/app/components/Banner.tsx
@@ -3,10 +3,9 @@ export default function Banner() {
     return (
         <div className="w-full bg-red-600 text-white text-center py-3 shadow-md animate-drop-in ">
             <p className="text-sm sm:text-base font-medium">
-                {/* <Link className="duration-300 hover:text-red-300 hover:underline" href="https://11.ugahacks.com/">
+                <Link className="duration-300 hover:text-red-300 hover:underline" href="https://11.ugahacks.com/">
                      UH11 Registration Live!
-                </Link> */}
-                ⚠️ Registration and event website for UGAHacks 11 are temporarily unavailable. We are actively working on a fix, thank you for your patience! ⚠️
+                </Link>
             </p>
         </div>
     );
